b2886b0547005b562f8b8c50368e963f57ed67b4
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-03-10 14:23:08 -0400

    Code changes made for printing Greeting from Member-1 and Group project setup

---
+++ Lab 6_TeamGreeting/src/info5100/team/greeting/Member1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package info5100.team.greeting;
+
+/**
+ *
+ * @author sravankumarkurapati
+ * The purpose of this class is to print greeting message from Member-1
+ */
+public class Member1 {
+    public static void main(String[] args){
+        System.out.println("Hello from Member 1!");
+    }
+}
